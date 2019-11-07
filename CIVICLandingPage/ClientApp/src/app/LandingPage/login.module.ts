2b795b50f4fe9ccb540b0f9c49b6832994945845
WaleedIqbal71 <mwaleediqbal71@gmail.com>
2019-11-07 17:56:18 +0500

    landing Page

---
+++ CIVICLandingPage/ClientApp/src/app/LandingPage/login.module.ts
@@ -0,0 +1,31 @@
+﻿import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login.component';
+
+@NgModule({
+    declarations: [
+        LoginComponent,
+        HomeComponent,
+        FooterComponent
+    ],
+    imports: [
+        RouterModule.forChild([
+            {
+                path: '', component: LoginComponent, children: [
+                    {
+                        path: '', component: HomeComponent
+                    }
+                ]
+            }
+        ])
+    ],
+    providers: [],
+    bootstrap: [LoginComponent]
+})
+
+export class LoginModule {
+
+}
